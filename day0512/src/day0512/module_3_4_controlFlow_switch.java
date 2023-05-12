db7dc017b3e7c30013031387110a84328b17d393
IOP_Mobile <IOP_Mobile@DESKTOP-7KCT31R>
2023-05-12 13:42:34 +0800

    break的作用

--- day0512/src/day0512/module_3_4_controlFlow_switch.java
+++ day0512/src/day0512/module_3_4_controlFlow_switch.java
@@ -5,12 +5,12 @@ import javax.swing.JOptionPane;
 public class module_3_4_controlFlow_switch {
 
 	public static void main(String[] args) {
+		
 		String monthStr = JOptionPane.showInputDialog(null,"請輸入月份","",JOptionPane.INFORMATION_MESSAGE);
 		int month = Integer.parseInt(monthStr);
 		int monthcase = (month>12||month<1)?4:((month==2)?3:(month>7)?((month%2==0)?1:2):((month%2==0)?2:1));
-		
 		switch(monthcase) {
-//		case 1,3,5,7,9:						//JDK14以上才能用
+		//case 1,3,5,7,9:						//jdk14以上才能用
 		case 1:
 			System.out.println("有31天");
 			break;
@@ -23,6 +23,8 @@ public class module_3_4_controlFlow_switch {
 		default:
 			break;
 		}
+		//不使用break的話，會在進入case之後不管case判斷，直接往下執行，直到break或是switch語句結束
+		
 
 	}
 

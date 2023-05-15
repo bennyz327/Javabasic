bd9c45c9b9e3284106f985de7b36bb3801374e38
IOP_Mobile <IOP_Mobile@DESKTOP-7KCT31R>
2023-05-15 16:43:57 +0800

    fix name usage

--- day0515/src/day0515/lab_addDemo/lab_5_3_useAdd.java
+++ day0515/src/day0515/lab_addDemo/lab_5_3_useAdd.java
@@ -4,7 +4,7 @@ public class lab_5_3_useAdd {
 
 	public static void main(String[] args) {
 		lab_5_3_addDemo addDemo=new lab_5_3_addDemo();		//先建立物件
-		int ans = lab_5_3_addDemo.addMD(8,7);				//使用方法
+		int ans = addDemo.addMD(8,7);				//使用方法
 		System.out.println(ans);
 
 	}

4700a5c13af3dc76fc29a302c1ec1c792ea03486
IOP_Mobile <IOP_Mobile@DESKTOP-7KCT31R>
2023-05-15 16:14:03 +0800

    加法方法範例

---
+++ day0515/src/day0515/lab_addDemo/lab_5_3_addDemo.java
@@ -0,0 +1,9 @@
+package day0515.lab_addDemo;
+
+public class lab_5_3_addDemo {
+
+    	public static int addMD(int a,int b) {
+    		return a+b;
+    	}
+
+}
---
+++ day0515/src/day0515/lab_addDemo/lab_5_3_useAdd.java
@@ -0,0 +1,12 @@
+package day0515.lab_addDemo;
+
+public class lab_5_3_useAdd {
+
+	public static void main(String[] args) {
+		lab_5_3_addDemo addDemo=new lab_5_3_addDemo();		//先建立物件
+		int ans = lab_5_3_addDemo.addMD(8,7);				//使用方法
+		System.out.println(ans);
+
+	}
+
+}

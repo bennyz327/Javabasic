447ba2f4c6f448e078d6064fa88a1bd1b771ad3e
IOP_Mobile <IOP_Mobile@DESKTOP-7KCT31R>
2023-05-15 16:33:18 +0800

    方法與靜態修飾子

---
+++ day0515/src/day0515/Module_5_3.java
@@ -0,0 +1,43 @@
+package day0515;
+
+public class Module_5_3 {
+	
+//建立簡單的方法並使用(同檔案)
+//    public static void main(String[] args) {					//main方法
+//        String Vegetables = "apple"; 							//定義要導入的變數
+//        Vegetables = Knife.dice(Vegetables);					//呼叫所需的方法
+//        System.out.println("Diced vegetables: " + Vegetables);	//輸出方法回傳的結果
+//    }
+//    static class Knife {										//定義Knife
+//    	//方法的定義法
+//    	/*[修飾子][回傳資料型態][方法名稱]([參數的資料型態][參數名稱])*/
+//    	static String dice(String vegetables) {					//定義Knife中的方法
+//    		/*return根據"回傳資料型態"回傳物件*/
+//    		return "Diced "+vegetables;							//定義方法的回傳值
+//    	}
+//	}
+   
+
+//建立簡單的方法並使用不同檔案
+//範例在addDemo
+//	
+//	
+//static修飾子用法與差別
+//範例在lab_staticTest
+//	
+//加上static的變數是靜態變數，會影響到所有變數
+	
+	/*
+	 * 靜態方法與物件方法的差異
+	 * 靜態方法:
+	 * 	1.不用建立物件
+	 * 	2.無法使用物件變數
+	 * 物件方法:
+	 * 	1.需先建立物件
+	 * 	2.可以使用靜態變數+物件變數
+	 * 
+	 * 因為物件建立後才會有物件變數，而靜態變數在變數建立前就已經配置在記憶體當中了
+	 * */
+	
+	
+}

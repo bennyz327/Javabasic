a7967e9f5fcb281940e42bbce906fd147430294c
IOP_Mobile <IOP_Mobile@DESKTOP-7KCT31R>
2023-05-15 16:37:59 +0800

    靜態宣告的差別的範例

---
+++ day0515/src/day0515/lab_staticTest/methodDemo.java
@@ -0,0 +1,7 @@
+package day0515.lab_staticTest;
+
+public class methodDemo {
+	int i1=100;
+	static int i2=100;
+
+}
---
+++ day0515/src/day0515/lab_staticTest/useDemo.java
@@ -0,0 +1,33 @@
+package day0515.lab_staticTest;
+
+public class useDemo {
+
+	public static void main(String[] args) {
+		
+//		//改變非static的變數可以獨立改變
+//		methodDemo a1=new methodDemo();
+//		methodDemo a2=new methodDemo();
+//		methodDemo a3=new methodDemo();
+//		System.out.println(a1.i1);
+//		System.out.println(a2.i1);
+//		System.out.println(a3.i1);
+//		a1.i1 = 500;
+//		System.out.println(a1.i1);
+//		System.out.println(a2.i1);
+//		System.out.println(a3.i1);
+//		
+//		//改變static的變數則會影響到其他物件
+//		methodDemo a1=new methodDemo();
+//		methodDemo a2=new methodDemo();
+//		methodDemo a3=new methodDemo();
+//		System.out.println(a1.i2);
+//		System.out.println(a2.i2);
+//		System.out.println(a3.i2);
+//		a1.i2 = 500;						//不管用類別還是用物件存取static變數，只要改變就會影響到其他物件
+//		System.out.println(a1.i2);
+//		System.out.println(a2.i2);
+//		System.out.println(a3.i2);		
+		
+	}
+
+}

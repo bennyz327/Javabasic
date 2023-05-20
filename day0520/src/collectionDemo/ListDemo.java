fb28bdf8d44d346e193ebe966b5d944b9f659ca2
IOP_Mobile <IOP_Mobile@DESKTOP-7KCT31R>
2023-05-20 14:53:20 +0800

    Generic 用法

--- day0520/src/collectionDemo/ListDemo.java
+++ day0520/src/collectionDemo/ListDemo.java
@@ -1,20 +1,24 @@
 package collectionDemo;
 
-//import java.util.ArrayList;
-//import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 //ctrl+shift+o	快速import
 
 public class ListDemo {
 
 	public static void main(String[] args) {
 
-		//還沒使用泛型
-		//String
-//		List list1=new ArrayList();
-//		list1.add("Apple");
-//		list1.add("Banana");
-//		list1.add("Cat");
-//		list1.add(1,"Duck");						//插入該索引值，後面的元素則往後推
+		List<String> list1=new ArrayList<>();		//加上泛型
+		list1.add("Apple");
+		list1.add("Banana");
+		list1.add("Cat");
+		list1.add(1,"Duck");						//插入該索引值，後面的元素則往後推
+		
+		for (String string : list1) {				//用泛型，就能指定物件類別為字串
+			System.out.println(string);
+		}
+		//list1.add(1.2);							//也能防止不同的資料型態加進來
+		
 		
 //		System.out.println(list1);
 //		System.out.println(list1.get(2));			//拿出索引值2的元素
@@ -29,9 +33,9 @@ public class ListDemo {
 		
 		
 //自動裝箱讓基本資料型別也可以直接放到List或使用其他的方法
-		Integer i=10;	//自動裝箱	(JDK>1.5)
-		int i2=i;		//自動拆箱
-		System.out.println(i.getClass().getName());
+//		Integer i=10;	//自動裝箱	(JDK>1.5)
+//		int i2=i;		//自動拆箱
+//		System.out.println(i.getClass().getName());
 	}
 
 }
